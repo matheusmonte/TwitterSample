0cb2e273f214e499c095dc5dca79c05d53d165a7
Matheus Clark dos S. Monte <ext-matheus.monte@microsoft.com>
2015-06-01 18:02:10 -0400

    [TwitterUtils]CReate and add TwitterUtils class
    Signed-off-by: Matheus Clark dos S. Monte <ext-matheus.monte@microsoft.com>

---
+++ ShareUtils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using Windows.System;
+
+namespace TwitterSample.Utils
+{
+    public static class TwitterUtils
+    {
+        private const string TWITTER_URL = "https://twitter.com/intent/tweet?text={0}&url={1}";
+
+        public static async void ShareOnTwitter(string Content, string Url)
+        {
+            string twitterUrl = string.Format(TWITTER_URL, new string[] { WebUtility.UrlEncode(Content), WebUtility.UrlEncode(Url) });
+            await Launcher.LaunchUriAsync(new Uri(twitterUrl));
+        }
+    }
+}
